079db8a421665720feab4cdf2bd5940b0aa3965a
Le Dinh Hoa <dinhhoa.0701@gmail.com>
2025-01-17 11:43:40 +0700

    fix http://localhost:8080 qua http://localhost:3000

--- app.mjs
+++ app.mjs
@@ -64,7 +64,7 @@ const App = async () => {
   if (process.env.NODE_ENV === "dev") {
     app.listen(8080, () => {
       console.log(
-        `Server is running on port 8080. Check the app on http://localhost:8080`
+        `Server is running on port 8080. Check the app on http://localhost:3000`
       );
     });
   }
