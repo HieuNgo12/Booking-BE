ab4820e692007ef792ed829e2b7d7ca0627fed51
Le Dinh Hoa <dinhhoa.0701@gmail.com>
2025-01-18 15:07:38 +0700

    Update AdminControllers.mjs

--- controllers/AdminControllers.mjs
+++ controllers/AdminControllers.mjs
@@ -245,6 +245,8 @@ const logIn = async (req, res, next) => {
 
       return res.status(200).json({
         message: "Log in successful!",
+        accessToken: accessToken,
+        refreshToken: refreshToken,
       });
     }
   } catch (error) {
